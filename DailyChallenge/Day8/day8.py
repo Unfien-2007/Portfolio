4d42ee31fcb2d9a41dd55cca869810ae51b48230
[Unfien-2007] <Unfienproductions@gmail.com>
2026-01-07 20:59:32 +0800

    Update Day 8 challenge: Adjust age calculation to determine when user will turn 25

--- DailyChallenge/Day8/day8.py
+++ DailyChallenge/Day8/day8.py
@@ -1,7 +1,11 @@
 #Input/Output
 #Ask the user for their name and age using `input()`.
-# Calculate the year they will turn 100 and print a sentence telling them.
+# Calculate the year they will turn 25 and print a sentence telling them.
 #
 #
+currentAge = int(input("Enter your age: "))
 
-
+currentYear = 2025
+yearTurn25 = currentYear + (25 - currentAge)
+print(f"You will turn 25 in the year {yearTurn25}.")
+print(f"you have {yearTurn25 - currentYear} years left to turn 25.")
