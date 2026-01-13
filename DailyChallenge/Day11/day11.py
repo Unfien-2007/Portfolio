9b1787f2d7e3fd258d99f0c80a6c6a22ce6398cf
[Unfien-2007] <Unfienproductions@gmail.com>
2026-01-13 18:14:21 +0800

    Fix user input handling and validation for size selection in Day 11 challenge

--- DailyChallenge/Day11/day11.py
+++ DailyChallenge/Day11/day11.py
@@ -3,3 +3,17 @@
 #If S cost is 100, M is 150, L is 200.
 #  Handle invalid inputs (e.g., user types "XL").
 
+
+UserInput = input("Input size(S, M, L) for price:")
+
+
+#logic
+if UserInput == "S":
+    print("size cost 100")
+elif UserInput == "M":
+    print("size cost 150")
+elif UserInput == "L":
+    print("size cost 200")
+elif UserInput != "S,M,L":
+    print("Invalid Input")
+
